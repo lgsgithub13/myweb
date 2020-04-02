689c1f6597fb1ff421a98843fbe7cec6d61e5843
lgsgithub13 <m18658756225@163.com>
2020-04-02 20:13:56 +0800

    0402

---
+++ js/time.js
@@ -0,0 +1,11 @@
+setInterval(function()
+{
+    var time=new Date();
+    var h,m,s,xs;
+    h=time.getHours();
+    m=time.getMinutes();
+    s=time.getSeconds();
+    xs=time.getMilliseconds();
+    var c=document.getElementById("T");
+    c.innerHTML=h+":"+m+":"+s+":"+xs;
+},0);
