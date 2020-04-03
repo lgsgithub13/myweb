4e9525bfabbc299e81e721864636bd979450cfd7
lgsgithub13 <m18658756225@163.com>
2020-04-03 21:31:00 +0800

    优化代码

--- js/addShuqian.js
+++ js/addShuqian.js
@@ -1,28 +1,28 @@
 function addShuQian()
 {
-	//alert("happy");
+	//创建标签并添加到body中
 	var body=document.getElementById("body");
 	var newTag=document.createElement("div");
-	//newTag.innerHTML="JAVA";
 	body.appendChild(newTag);
-	//newTag.style.background="red";
 	newTag.setAttribute("className","box");
 	newTag.style="position:relative;display:inline-block;margin:0.5em;";
-	var link=document.createElement("a");
+	//创建标签内的子标签并添加
+	var a=document.createElement("a");
 	var img=document.createElement("img");
-	var text=document.createElement("p");
-	var pimg=document.createElement("p");
-	newTag.appendChild(link);
-	newTag.appendChild(pimg);
-	newTag.appendChild(text);
-	pimg.appendChild(img);
-	text.innerHTML="未命名";
-	text.setAttribute("className","text");
-	text.style="font-size:1em;";
+	var p=document.createElement("p");
+	var p_include_img=document.createElement("p");
+	newTag.appendChild(a);
+	newTag.appendChild(p_include_img);
+	newTag.appendChild(p);
+	//为子标签添加属性和样
+	p_include_img.appendChild(img);
+	p.innerHTML="开发中";
+	p.setAttribute("className","text");
+	p.style="font-size:1em;";
 	img.setAttribute("className","image");
 	img.setAttribute("src","../test.png");
 	img.style="width:3em;height:3em;";
-	link.setAttribute("className","link");
-	link.setAttribute("href","x:sc");
-	link.style="width:100%;height:100%;position:absolute;right:0px;bottom:0px;";
+	a.setAttribute("className","link");
+	a.setAttribute("href","x:sc");
+	a.style="width:100%;height:100%;position:absolute;right:0px;bottom:0px;";
 }
